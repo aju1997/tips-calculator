cbae9ff3835886a731540b9617e6b2147fdf46a4
Andy Ju <andyju1997@gmail.com>
2018-06-29 23:10:56 -0700

    Initial Commit

---
+++ Tips Calculator/FinalAmount.swift
@@ -0,0 +1,52 @@
+//
+//  FinalAmount.swift
+//  Tips Calculator
+//
+//  Created by Andy Ju on 6/26/18.
+//  Copyright © 2018 Andy Ju. All rights reserved.
+//
+
+import UIKit
+
+class FinalAmount: UIViewController {
+    
+   
+    
+    @IBOutlet weak var enteredAmount: UILabel!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    
+    var userAmountPassed: String = ""
+    var userTipAmountPassed: String = ""
+    var userTotalAmountPassed: String = ""
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        enteredAmount?.text = userAmountPassed
+        tipAmount?.text =  userTipAmountPassed
+        totalAmount?.text = userTotalAmountPassed
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func resetBtn(_ sender: Any) {
+        performSegue(withIdentifier: "MoveToInitial", sender: self)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tips Calculator/InitialUserScreen.swift
@@ -0,0 +1,76 @@
+//
+//  InitialUserScreen.swift
+//  Tips Calculator
+//
+//  Created by Andy Ju on 6/26/18.
+//  Copyright © 2018 Andy Ju. All rights reserved.
+//
+
+import UIKit
+
+class InitialUserScreen: UIViewController {
+
+    @IBOutlet weak var userAmount: UITextField!
+    @IBOutlet weak var userTipPercentage: UITextField!
+    
+    var stringTipAmount = String()
+    var stringTotalAmount = String()
+    var stringUserAmount = String()
+    
+    var doubleAmount: Double = 0.0
+    var doubleTipPercentage: Double = 0.0
+    var doubleTotalAmount: Double = 0.0
+    var doubleTipAmount: Double = 0.0
+    
+    
+    
+    func stringToDouble() {
+        doubleAmount = Double(userAmount.text!)!
+        doubleTipPercentage = Double(userTipPercentage.text!)!
+        
+        doubleTipPercentage *= 0.01
+        
+        doubleTipAmount = doubleAmount * doubleTipPercentage
+        
+        doubleTotalAmount = doubleAmount + doubleTipAmount
+    }
+    
+    func doubleToString() {
+        stringTipAmount = String(format: "%.2f", doubleTipAmount)
+        stringTotalAmount = String(format: "%.2f", doubleTotalAmount)
+        stringUserAmount = String(format: "%.2f", doubleAmount)
+    }
+    
+    
+    //Passing text field data to FinalAmount VC
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        stringToDouble()
+        doubleToString()
+        if let vcInitial = segue.destination as? FinalAmount {
+            vcInitial.userTipAmountPassed = stringTipAmount
+            vcInitial.userTotalAmountPassed = stringTotalAmount
+            vcInitial.userAmountPassed = stringUserAmount
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        if !((userAmount.text?.isEmpty)!) && !((userTipPercentage.text?.isEmpty)!) {
+            performSegue(withIdentifier: "MoveToFinal", sender: self)
+        }
+    }
+
+}
+
